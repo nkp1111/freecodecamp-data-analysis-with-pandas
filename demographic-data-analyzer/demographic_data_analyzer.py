443c176235e2d0f8add899b3e9885587c4d7b41c
nkp1111 <neerajp4321@gmail.com>
2023-01-10 18:21:16 +0530

    Feat: add higher and lower education high earning

--- demographic-data-analyzer/demographic_data_analyzer.py
+++ demographic-data-analyzer/demographic_data_analyzer.py
@@ -19,17 +19,24 @@ def calculate_demographic_data(print_data=True):
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
     high_edu = ["Bachelors", "Masters", "Doctorate"]
-    higher_education = len(df.loc[
-                           ((df.education == "Bachelors") |
-                            (df.education == "Masters") |
-                            (df.education == "Doctorate")) &
-                           (df.salary == ">50K")])
 
-    lower_education = None
+    higher_education = df.loc[(df.education == "Bachelors") |
+                              (df.education == "Masters") |
+                              (df.education == "Doctorate")]
+
+    lower_education = df.loc[
+        (df.education != "Bachelors") &
+        (df.education != "Masters") &
+        (df.education != "Doctorate")]
 
     # percentage with salary >50K
-    higher_education_rich = round(higher_education * 100 / len(df), 1)
-    lower_education_rich = None
+    higher_education_rich = round(
+        len(higher_education.loc[higher_education.salary == ">50K"]) * 100 /
+        len(higher_education), 1)
+
+    lower_education_rich = round(
+        len(lower_education.loc[lower_education.salary == ">50K"]) * 100 /
+        len(lower_education), 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = None
@@ -54,14 +61,17 @@ def calculate_demographic_data(print_data=True):
         print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
         print(
             f"Percentage with higher education that earn >50K: {higher_education_rich}%")
-        print(
-            f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+
+        print(f"Percentage without higher education that earn >50K: {lower_education_rich}%"
+              )
         print(f"Min work time: {min_work_hours} hours/week")
         print(
-            f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+            f"Percentage of rich among those who work fewest hours: {rich_percentage}%"
+        )
         print("Country with highest percentage of rich:", highest_earning_country)
         print(
-            f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
+            f"Highest percentage of rich people in country: {highest_earning_country_percentage}%"
+        )
         print("Top occupations in India:", top_IN_occupation)
 
     return {
