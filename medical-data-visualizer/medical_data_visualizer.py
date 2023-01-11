5fdac0deed1d2eb3ec6e8336afc86c1b48ceaf2f
nkp1111 <neerajp4321@gmail.com>
2023-01-11 15:30:54 +0530

    Feat: add catplot

--- medical-data-visualizer/medical_data_visualizer.py
+++ medical-data-visualizer/medical_data_visualizer.py
@@ -5,7 +5,6 @@ import numpy as np
 
 # Import data
 df = pd.read_csv("medical_examination.csv")
-print(df.head())
 
 # Add 'overweight' column
 df['overweight'] = np.where(
@@ -20,15 +19,22 @@ df["cholesterol"] = np.where(df["cholesterol"] == 1, 0, 1)
 
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    df_cat = pd.melt(df, value_vars=[
+                     "cholesterol", "gluc", "smoke", "alco", "active", "overweight"], id_vars=["cardio"])
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
+    df_cat["sum_total"] = np.arange(len(df_cat))
+    df_cat = df_cat.groupby(
+        ["cardio", "variable", "value"], as_index=False).count()
+
+    print(df_cat)
 
     # Draw the catplot with 'sns.catplot()'
+    plot = sns.catplot(data=df_cat, x="variable", y="sum_total",
+                       col="cardio", kind="bar", legend_out=True, hue="value")
 
     # Get the figure for the output
-    fig = None
+    fig = plot.fig
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
@@ -38,7 +44,8 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df.loc[df["ap_lo"] <= df["ap_hi"] & df["height"] >= df["height"].quantile(0.025) & df["height"] <= df["height"].quantile(
+        0.975) & df["weight"] >= df["weight"].quantile(0.025) & df["weight"] <= df["weight"].quantile(0.975)]
 
     # Calculate the correlation matrix
     corr = None
